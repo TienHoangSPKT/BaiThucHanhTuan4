d85f01a569e856a4eb7cdb1cb7dc2f9b9ea1303c
VTH11 <hoangvthanh0211@gmail.com>
2025-09-17 10:07:42 +0700

    xong

---
+++ Bai1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Bai1 
+{
+    public static void main(String[] args)
+    {
+    Scanner i =new Scanner(System.in);
+    System.out.print ("Nhap so can kiem tra : ");
+    int so = i.nextInt();
+    if(so==0)
+    {
+        System.out.println("So vua nhap la so 0");
+    }
+    else
+    {
+        if (so<0) {
+            System.out.println("So vua nhap la so am");
+        }
+        else
+        {
+            System.out.println("So vua nhap la so duong");
+        }
+    }
+    i.close();
+    }
+}
